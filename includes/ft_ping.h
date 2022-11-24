873ed5b76f05fc45fbeabc522246d771e588d027
3rdn4x3l4 <alagache@student.42.fr>
2022-11-24 17:51:55 +0100

    add gitignore for pacaps and moved functions according to new subjects

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <bits/types/struct_timeval.h>
 #include <netdb.h>
 #include <signal.h>
 #include <stdarg.h>
@@ -10,27 +11,21 @@
 #include <unistd.h>
 
 
-//getpid;
-
-
-pid_t getpid(void);
-
-// pid_t getppid(void);
-
-
-//getuid;
+//getpid getuid;
 
 
 uid_t getuid(void);
-uid_t geteuid(void);
+pid_t getpid(void);
 
 
-//getaddrinfo;
+//getaddrinfo freeaddrinfo gai_strerror;
 
 
 int getaddrinfo(const char *node, const char *service,
 		const struct addrinfo *hints,
 		struct addrinfo **res);
+void freeaddrinfo(struct addrinfo *res);
+const char *gai_strerror(int errcode);
 
 // struct addrinfo {
 // 	int              ai_flags;
@@ -43,8 +38,6 @@ int getaddrinfo(const char *node, const char *service,
 // 	struct addrinfo *ai_next;
 // };
 
-// void freeaddrinfo(struct addrinfo *res);
-// const char *gai_strerror(int errcode);
 
 
 //gettimeofday;
@@ -52,18 +45,12 @@ int getaddrinfo(const char *node, const char *service,
 
 // int gettimeofday(struct timeval *tv, struct timezone *tz);
 
-// int settimeofday(const struct timeval *tv, const struct timezone *tz);
 
-
-//inet_ntop;
+//inet_ntop inet_pton;
 
 
 const char *inet_ntop(int af, const void *src,
 		char *dst, socklen_t size);
-
-
-//inet_pton;
-
 
 int inet_pton(int af, const char *src, void *dst);
 
@@ -74,38 +61,27 @@ int inet_pton(int af, const char *src, void *dst);
 void exit(int status);
 
 
-//signal;
+//alarm signal;
 
 
 typedef void (*sighandler_t)(int);
 sighandler_t signal(int signum, sighandler_t handler);
-
-
-//alarm;
-
-
 unsigned int alarm(unsigned int seconds);
 
-//setsockopt;
 
+//socket setsockopt;
+
+
+int socket(int domain, int type, int protocol);
 int setsockopt(int sockfd, int level, int optname,
 		const void *optval, socklen_t optlen);
-
 // int getsockopt(int sockfd, int level, int optname,
 // 		void *optval, socklen_t *optlen);
 
 
-//recvmsg;
 
 
-ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags);
-
-// ssize_t recv(int sockfd, void *buf, size_t len, int flags);
-// ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
-// 		struct sockaddr *src_addr, socklen_t *addrlen);
-
-
-//sendto;
+//sendto recvmsg;
 
 
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
@@ -114,11 +90,13 @@ ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
 // ssize_t send(int sockfd, const void *buf, size_t len, int flags);
 // ssize_t sendmsg(int sockfd, const struct msghdr *msg, int flags);
 
+ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags);
 
-//socket;
+// ssize_t recv(int sockfd, void *buf, size_t len, int flags);
+// ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
+// 		struct sockaddr *src_addr, socklen_t *addrlen);
 
 
-int socket(int domain, int type, int protocol);
 
 
 //xprintf;
@@ -129,8 +107,8 @@ int fprintf(FILE *stream, const char *format, ...);
 int dprintf(int fd, const char *format, ...);
 int sprintf(char *str, const char *format, ...);
 int snprintf(char *str, size_t size, const char *format, ...);
-int vprintf(const char *format, va_list ap);
-int vfprintf(FILE *stream, const char *format, va_list ap);
-int vdprintf(int fd, const char *format, va_list ap);
-int vsprintf(char *str, const char *format, va_list ap);
-int vsnprintf(char *str, size_t size, const char *format, va_list ap);
+// int vprintf(const char *format, va_list ap);
+// int vfprintf(FILE *stream, const char *format, va_list ap);
+// int vdprintf(int fd, const char *format, va_list ap);
+// int vsprintf(char *str, const char *format, va_list ap);
+// int vsnprintf(char *str, size_t size, const char *format, va_list ap);
--- srcs/main.c
+++ srcs/main.c
@@ -1,6 +1,29 @@
 #include "ft_ping.h"
+#include "libft.h"
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 int	main(void)
 {
-  return 0;
+	struct timeval  tv;
+	struct timezone  tz;
+	in_addr_t  add;
+	char buf[INET_ADDRSTRLEN];
+	int ret;
+
+	ft_memset(&tv, 0, sizeof(tv));
+	ft_memset(&add, 0, sizeof(add));
+	ft_memset(&tz, 0, sizeof(tz));
+	printf("%ld %ld\n", tv.tv_sec, tv.tv_usec);
+	printf("%d %d\n", tz.tz_dsttime, tz.tz_minuteswest);
+
+	ret = gettimeofday(&tv, &tz);
+	printf("ret = %d\n", ret);
+	printf("%ld %ld\n", tv.tv_sec, tv.tv_usec);
+	printf("%d %d\n", tz.tz_dsttime, tz.tz_minuteswest);
+
+	inet_ntop(AF_INET, &add, buf, INET_ADDRSTRLEN);
+	printf("%s\n", buf);
+
+	return 0;
 }
