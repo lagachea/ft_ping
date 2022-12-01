58c6333df795712cb3ece20ccf9e069e0b96f179
alagache <alagache@student.42.fr>
2022-12-01 14:56:06 +0100

    some python

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
-import socket
+import socket as sckt
 import os
 
+getLocalAddress = False
 pid = os.getpid()
 uid = os.getuid()
+family, socktype, protocol = sckt.AF_INET, sckt.SOCK_RAW, sckt.IPPROTO_TCP
 if uid != 0:
+    getLocalAddress = True
     print('Cannot use raw socket when not root')
-    exit(1);
+    exit(1)
+
 # addrs = ["9.9.9.9","www.42.fr","google.com"]
 # addr_infos = [socket.getaddrinfo(addr, "") for addr in addrs]
 # print(addr_infos)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+sock = sckt.socket(family, socktype, protocol)
+print(sock)
+sock.setsockopt(sckt.IPPROTO_IP, sckt.IP_HDRINCL, 1)
 
-src_addr = socket.inet_pton(socket.AF_INET, "172.20.76.6")
+src_addr = sckt.inet_pton(sckt.AF_INET, "172.20.76.6")
 ip_header = b'\x45\x00\x00\x1c' # Version, IHL, Type of Service | Total Length
 ip_header += b'\xab\xcd\x00\x00' # Identification | Flags, Fragment Offset
 ip_header += b'\x40\x01\x6b\xd8' # TTL, Protocol | Header Checksum
@@ -24,6 +29,14 @@ icmp_header = b'\x08\x00\xe5\xca' # Type of message, Code | Checksum
 icmp_header += b'\x12\x34\x00\x01' # Identifier | Sequence Number
 
 packet = ip_header + icmp_header
-# s.sendto(packet, ('8.8.8.8', 0))
-s.connect(('8.8.8.8',0))
-print(s.getsockname())
+
+# This sends our packet to google '8.8.8.8'
+sock.sendto(packet, ('8.8.8.8', 0))
+if getLocalAddress is False:
+    # socktype = sckt.SOCK_DGRAM
+    exit(0)
+
+# To be able to fetch our local address from the socket we need connect to the
+# remote address
+sock.connect(('8.8.8.8',0))
+print(sock.getsockname())
