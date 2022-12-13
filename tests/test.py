1684d1d97eb3387e29acd2077c828d11f07f8fc3
alagache <alagache@student.42.fr>
2022-12-14 00:58:43 +0100

    added a small print of launched cmd

--- tests/test.py
+++ tests/test.py
@@ -7,6 +7,9 @@ to_str = str(timeout) + "s"
 original = "ping"
 to_test = "./ft_ping"
 
+def printCmd(args: list):
+    print(f"Launching with following arguments\n{args}")
+
 def getArgs(name: str) -> list:
     args = [
         "timeout",
@@ -20,15 +23,22 @@ def getArgs(name: str) -> list:
 
 def getTArgs(name: str) -> list:
     args = getArgs(name)
-    args[-2] = to_test
+    last = args.pop()
+    args.append("sudo")
+    args.append(to_test)
+    args.append(last)
     return args
 
 for hname in hostnames:
     # do the original
     args = getArgs(hname)
+    printCmd(args)
     with subprocess.Popen(args, stdout=subprocess.PIPE) as ping:
         ping.communicate()
         # what to pipe to to be able to compare output with 
 
     # do the same with to test ping
     targs = getTArgs(hname)
+    printCmd(targs)
+    with subprocess.Popen(args) as test:
+        test.communicate()
