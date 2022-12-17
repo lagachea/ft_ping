de697d50c90912914970c2b66fe2b849c68dc7c1
alagache <alagache@student.42.fr>
2022-12-17 05:45:31 +0100

    upgraded firewall with switch case_dic/switch_func

--- tests/firewall.py
+++ tests/firewall.py
@@ -6,14 +6,43 @@ import subprocess, shlex
 def getcmd(cmd: str) -> list[str]:
     return shlex.split(cmd)
 
-# (flush start) firewall
-flush_cmd = "iptables -F"
-subprocess.run(getcmd(flush_cmd))
-print('FLUSHED FIREWAL RULES')
+def restore():
+    # (flush start) firewall
+    flush_cmd = "iptables -F"
+    subprocess.run(getcmd(flush_cmd))
+    print('Restored firewall rules')
+
+def incoming():
+    incoming = "iptables -I INPUT -p icmp -j DROP"
+    print('\nSetting incoming rules')
+    subprocess.run(getcmd(incoming))
+
+
+def outgoing():
+    outgoing = "iptables -I OUTPUT -p icmp -j DROP"
+    print('\nSetting outgoing rules')
+    subprocess.run(getcmd(outgoing))
+
+def nothing():
+    print('\nNo rules added')
+
+cases = {
+    1: outgoing,
+    2: incoming,
+    3: nothing,
+}
+
+def switch():
+    to_run = cases.get(target)
+    if to_run is not None:
+        to_run()
+    return
+
+restore()
 
 target = False
 while not target:
-    target_nbr = input("1 to toggle outgoing packets\n2 to toggle incoming packets\n3 to restore\n")
+    target_nbr = input("1 to stop outgoing packets\n2 to stop incoming packets\n3 to stop no packets\n")
     try:
         target = int(target_nbr)
         if not 1 <= target <= 3:
@@ -22,18 +51,7 @@ while not target:
     except ValueError:
         print("Erro")
 
-incoming = "iptables -I INPUT -p icmp -j DROP"
-outgoing = "iptables -I OUTPUT -p icmp -j DROP"
-
-if target == 1:
-    print('\nSetting outgoing rules')
-    subprocess.run(getcmd(outgoing))
-elif target == 2:
-    print('\nSetting incoming rules')
-    subprocess.run(getcmd(incoming))
-elif target == 3:
-    print('\nNo rules added')
-    exit(0)
+switch()
 
 list_cmd = getcmd('iptables -S')
 with subprocess.Popen(list_cmd, stdout=subprocess.PIPE) as list_rules:
@@ -41,3 +59,5 @@ with subprocess.Popen(list_cmd, stdout=subprocess.PIPE) as list_rules:
     grep_cmd = getcmd('grep DROP')
     with subprocess.Popen(grep_cmd, stdin=list_rules.stdout) as grep_rules:
         grep_rules.communicate()
+
+exit(0)
