571357b1ef322045ee9d3503dac884d2149b89b0
alagache <alagache@student.42.fr>
2023-03-09 19:49:33 +0100

    refacto setRawSocket to use a local socket pointer

--- srcs/socket.c
+++ srcs/socket.c
@@ -11,13 +11,15 @@ int setSocket(t_socket *sckt) {
 void setRawSocket() {
   int res;
   struct icmp_filter filter;
+  t_socket *sock_ptr;
   int yes = 1;
 
-  g_ping->socket.family = AF_INET;
-  g_ping->socket.socktype = SOCK_RAW;
-  g_ping->socket.protocol = IPPROTO_ICMP;
+  sock_ptr = &g_ping->socket;
+  sock_ptr->family = AF_INET;
+  sock_ptr->socktype = SOCK_RAW;
+  sock_ptr->protocol = IPPROTO_ICMP;
 
-  res = setSocket(&g_ping->socket);
+  res = setSocket(sock_ptr);
   if (res == FAILURE) {
     printError("ERROR: %s | Failed getting a socket\n", strerror(errno));
     exit(FAILURE);
@@ -26,14 +28,14 @@ void setRawSocket() {
   // ICMP_FILTER use a bitmask of the ICMP_CODE (ex: 1 << ICMP_ECHO)
   // ICMP_ECHOREPLY is code 0 so all bits are set except the lowest one
   filter.data = 0xFFFFFFFEu;
-  res = setsockopt(g_ping->socket.sockfd, SOL_RAW, ICMP_FILTER, &filter, sizeof(filter));
+  res = setsockopt(sock_ptr->sockfd, SOL_RAW, ICMP_FILTER, &filter, sizeof(filter));
   if (res == -1) {
     printError("ERROR: %s | Failed setting a socket option\n", strerror(errno));
     exit(FAILURE);
   }
 
   // able to send to broadcast address
-  res = setsockopt(g_ping->socket.sockfd, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(yes));
+  res = setsockopt(sock_ptr->sockfd, SOL_SOCKET, SO_BROADCAST, &yes, sizeof(yes));
   if (res == -1) {
     printError("ERROR: %s | Failed setting a socket option\n", strerror(errno));
     exit(FAILURE);
