543c5fbaeb51ba6476992d2d44e202017b55e746
alagache <alagache@student.42.fr>
2023-03-09 19:55:16 +0100

    test correction

--- tests/test.c
+++ tests/test.c
@@ -1,60 +1,51 @@
-
-#include <stdio.h>
 #include "test.h"
 #include "ft_ping.h"
+#include <stdio.h>
 
 #ifdef DEBUG
-int __lsan_is_turned_off()
-{
-    return 1;
-}
+int __lsan_is_turned_off() { return 1; }
 #endif
 
- 
 t_ftping *g_ping;
- int tests_run = 0;
+int tests_run = 0;
 
- 
- static char * test_sockets() {
-     mu_assert("erro socket pas bon", 1 == 0);
+static char *test_sockets() { mu_assert("erro socket pas bon", 1 == 0); }
+
+static char *test_socket() {
+  // prequisite
+  // a = b
+  setRawSocket();
+
+  // assert
+  // a == b
+  mu_assert("erro socket empty", g_ping->socket.family == AF_INET);
+  mu_assert("erro socket empty", g_ping->socket.socktype == SOCK_RAW);
+  mu_assert("erro socket empty", g_ping->socket.protocol == IPPROTO_ICMP);
+  return 0;
 }
 
- static char * test_socket() {
-	 //prequisite
-	 //a = b
-	 getRawSocket();
+static char *all_tests() {
+  mu_run_test(test_socket);
+  mu_run_test(test_sockets);
+  return 0;
+}
 
-	 //assert
-	 //a == b
-     mu_assert("erro socket empty", g_ping->socket.family == AF_INET);
-     mu_assert("erro socket empty", g_ping->socket.socktype == SOCK_RAW);
-     mu_assert("erro socket empty", g_ping->socket.protocol ==  IPPROTO_ICMP);
-     return 0;
- }
- 
- static char * all_tests() {
-     mu_run_test(test_socket);
-     mu_run_test(test_sockets);
-     return 0;
- }
- 
- int main(int argc, char **argv) {
-	 (void) argc;
-	 (void) argv;
-	t_ftping pingdata;
+int main(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  t_ftping pingdata;
 
-	g_ping = &pingdata;
+  g_ping = &pingdata;
 
-	setup();
+  setup();
 
-     char *result = all_tests();
-     if (result != 0) {
-         printf("%s\n", result);
-     }
-     else {
-         printf("ALL TESTS PASSED\n");
-     }
-     printf("Tests run: %d\n", tests_run);
- 
-     return result != 0;
- }
+  char *result = all_tests();
+  if (result != 0) {
+    printf("%s\n", result);
+  } else {
+    printf("ALL TESTS PASSED\n");
+  }
+  printf("Tests run: %d\n", tests_run);
+
+  return result != 0;
+}
